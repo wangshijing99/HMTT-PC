c1c1e204751d3d8910a8b4402121cfb372b50ccc
wangshijing99 <413361719@qq.com>
2019-10-25 17:43:01 +0800

    使用vue-router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入element-ui
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+// 引入路由  @是webpack指定的路径别名，绝对路径，只有在js中使用
+import router from '@/router'
+// import router from './router/index.js'
 
 Vue.config.productionTip = false
+// 使用element-ui
 Vue.use(ElementUI)
 
 new Vue({
+  // 挂载路由到根实例
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+// 引入vue和vue-router
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+// 使用vue-router
+Vue.use(VueRouter)
+
+// 实例化一个路由
+const router = new VueRouter({
+  // 配置对象
+  // 路由规则选项 routes  若干规则
+  routes: []
+})
+
+// 导出路由
+export default router
