0ddae075620cd54372084ccbb3b70a4d9a83972b
wangshijing99 <413361719@qq.com>
2019-10-28 20:04:35 +0800

    首页模块基础布局完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,14 @@
 // 引入vue和vue-router
 import VueRouter from 'vue-router'
 import Vue from 'vue'
-// 引入组件login.vue
+// 引入组件login
 import Login from '@/views/login/index.vue'
+// 引入组件home
+import Home from '@/views/home'
+// 引入组件welcome
+import Welcome from '@/views/welcome'
+// 引入组件404
+import NotFound from '@/views/404'
 
 // 使用vue-router
 Vue.use(VueRouter)
@@ -13,8 +19,18 @@ const router = new VueRouter({
   // 路由规则选项 routes  若干规则
   routes: [{
     // 配置登录页面路由规则
-    path: '/login', component: Login
-  }]
+    path: '/login',
+    component: Login
+  },
+  { path: '/',
+    component: Home,
+    children: [
+      // 欢迎
+      { path: '/', component: Welcome }
+    ] },
+  // 匹配  不符合路由规则的路径
+  { path: '*', component: NotFound }
+  ]
 })
 
 // 导出路由
